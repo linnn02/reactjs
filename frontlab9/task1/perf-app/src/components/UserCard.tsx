0ef886ba7c897592e61ca44d63b1933f3f345149
linnn02 <adelina.love.game@gmail.com>
2026-04-18 01:33:10 +0500

    feat: add UserCard with React.memo optimization

---
+++ frontlab9/task1/perf-app/src/components/UserCard.tsx
@@ -0,0 +1,21 @@
+import { memo } from "react";
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+interface UserCardProps {
+    user: User;
+}
+
+export const UserCard = memo(function UserCard({ user }: UserCardProps) {
+    console.log("UserCard render");
+
+    return (
+        <div className="card">
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+        </div>);
+});
